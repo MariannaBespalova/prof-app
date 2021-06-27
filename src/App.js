76df7bdc85cd10d031ae69c593baa82f6c16914e
marianna <mariannabespalova9@gmail.com>
2021-06-27 19:06:55 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import './App.css';
+import Employees from 'components/Employees';
+import Jobs from 'components/Jobs';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
 
 function App() {
-  return (
-    <div >
+  const classes = useStyles();
 
+  return (
+    <div className={classes.root}>
+      <Jobs />
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
+        <Employees />
+      </main>
     </div>
   );
 }
